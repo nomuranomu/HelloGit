d23f3dabcd62f0f88f32e4b83de36434e389f0ee
Hitomi Nomura <h.nananbu@gmail.com>
2016-09-06 16:43:33 +0900

    てす２

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i, num;
+	int *a;
+
+	/* @@@ write your code below @@@ */
+	
+	printf("配列の要素数の入力："); 
+	scanf("%d", &num);
+	
+	a = (int*) malloc(sizeof(int)*num);
+	
+	if(a == NULL){
+		printf("メモリ確保失敗\n");
+		return 1;
+	}
+	
+	for(i=0; i<num; i++){
+		a[i] = i;
+		printf("%d ", a[i]);
+	}
+	free(a);	
+
+	/* @@@ wite your code above */
+
+	return 0;
+}
